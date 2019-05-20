5f639fb608339299d547ea025a39a0f37a3acfe9
Maruf <marufbinawal@gmail.com>
2019-05-20 12:52:39 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,25 +1,28 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand 1..11
 end
 
 def display_card_total
-  # code #display_card_total here
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  print "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  puts "Please show your cards "
+number = gets.chomp
+puts "You have #{number} "
 end
 
 def end_game
-  # code #end_game here
+   if number > 21 
+    puts "Sorry, you hit#{number}. Thanks for playing!"
 end
 
 def initial_round
